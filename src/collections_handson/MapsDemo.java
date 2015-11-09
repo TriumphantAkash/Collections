38b2c5f0363111fa8926bdee46aae7db6d91a4f3
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2015-11-08 20:37:11 -0600

    HashMap, LinkedHashMap and TreeMaps

--- src/collections_handson/MapsDemo.java
+++ src/collections_handson/MapsDemo.java
@@ -3,30 +3,39 @@ package collections_handson;
 import java.util.HashMap;
 import java.util.Map;
 
+class Temp {}
+
 public class MapsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
 		
-		map.put(5, "five");
-		map.put(6, "six");
-		map.put(7, "seven");
-		map.put(8, "eight");
-		map.put(9, "nine");
-		map.put(10, "ten");
-		map.put(8, "duplicate_eight");
+		hashMap.put(5, "five");
+		hashMap.put(6, "six");
+		hashMap.put(7, "seven");
+		hashMap.put(8, "eight");
+		hashMap.put(9, "nine");
+		hashMap.put(10, "ten");
+		hashMap.put(8, "duplicate_eight");
 		
-		String number = map.get(8);
+		String number = hashMap.get(8);
 		System.out.println(number);
 		
 		System.out.println("******************************");
 		
-		for(Map.Entry<Integer, String> entry: map.entrySet()) {
+		for(Map.Entry<Integer, String> entry: hashMap.entrySet()) {
 			entry.getKey();
 			entry.getValue();
 			System.out.println(entry+"\n");
 		}
+		
+		
+		
+		System.out.println(new Temp());
+		
+		//*NOTE: HashMap does not keep the entries in order,
+		//		 You may get a different order every time iterating over the HashMap
 		
 	}
 
---
+++ src/collections_handson/SomeMoreMaps.java
@@ -0,0 +1,48 @@
+package collections_handson;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class SomeMoreMaps {
+
+	private static HashMap<Integer, String> hashMap;
+	private static LinkedHashMap<Integer, String> linkedHashMap;
+	private static TreeMap<Integer, String> treeMap;
+	
+	public static void main(String[] args) {
+		hashMap = new HashMap<Integer, String>();
+		linkedHashMap = new LinkedHashMap<Integer, String>();
+		treeMap = new TreeMap<Integer, String>();
+		
+		
+		//iterating over the map
+//		for (Map.Entry<Integer, String> entry: hashMap.entrySet()) {
+//			System.out.println(entry.getKey() + ":" + entry.getValue());
+//		}
+		
+		testMap(hashMap);	//HashMap does not guarantee any particular order in which it has to be stored
+		testMap(linkedHashMap);	//LinkedHashMap stores the data in the same order they were inserted in the map
+		testMap(treeMap);	//TreeMap sorts the entries according to the Key value in each entry
+	}
+	
+	//a method to check the basic difference between these 3 kinds of maps
+	public static void testMap(Map<Integer, String> passedMap) {
+		// adding data to the map
+		passedMap.put(3, "three");
+		passedMap.put(1, "one");
+		passedMap.put(7, "seven");
+		passedMap.put(2, "two");
+		passedMap.put(4, "four");
+		passedMap.put(5, "five");
+		passedMap.put(6, "six");
+		passedMap.put(8, "eight");
+		
+		for(Integer key: passedMap.keySet()) {
+			System.out.println(key + ":" + passedMap.get(key));
+		}
+		
+		System.out.println("********************************************************");
+	}
+
+}
