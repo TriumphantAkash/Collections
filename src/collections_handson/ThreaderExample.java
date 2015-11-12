a040dc1fb3de2d68fcf08cb800674e590b99e74c
Akash Chaturvedi <axc144430@utdallas.edu>
2015-11-12 09:35:39 -0600

    unknown changes from office computer

---
+++ src/collections_handson/ThreaderExample.java
@@ -0,0 +1,15 @@
+package collections_handson;
+
+public class ThreaderExample extends Thread {
+String a ;
+	public ThreaderExample(String a){
+		this.a = a;
+	}
+	public void run(){
+		int i =0;
+		while(i<10){
+			System.out.print(a);
+			i++;
+		}
+	}
+}
---
+++ src/collections_handson/sdasd.java
@@ -0,0 +1,12 @@
+package collections_handson;
+
+public class sdasd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ThreaderExample("A").start();
+
+		new ThreaderExample("B").start();
+	}
+
+}
